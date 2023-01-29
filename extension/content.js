cca8d910bb095b4e4c7a654e688b430dd20139f8
Ritik Mishra <ritik.mishra314@gmail.com>
2023-01-29 12:55:18 -0600

    real crappy fix highlighting

--- extension/content.js
+++ extension/content.js
@@ -34,46 +34,116 @@ window.onload = async (event) => {
   console.log(respdata)
 
   const checkstroverlap = (str1, str2) => {
-    if(!str1 || !str2) return 0;
-    const min_len = Math.min(str1.length, str2.length);
-    for(let i = 1; i < min_len; i++) {
-      let last_i_of_str1 = str1.substring(str1.length - i);
-      let first_i_of_str2 = str2.substring(0, i);
-      if (first_i_of_str2 == last_i_of_str1) {
-        return i
+    if (!str1 || !str2) return 0;
+
+    for (let str2_offsetfromstr1 = 0; str2_offsetfromstr1 < str1.length; str2_offsetfromstr1++) {
+      let str1_sub = str1.substring(str2_offsetfromstr1, str2_offsetfromstr1 + str2.length);
+      let str2_sub = str2.substring(0, str1.length - str2_offsetfromstr1)
+      if (str1_sub == str2_sub) {
+        return true;
       }
     }
-    return 0;
+    return false;
   }
 
   let recursive_find_replace = (node, find, parent = null) => {
     const forwardoverlap = checkstroverlap(find.og_text.toLowerCase().trim(), (node.innerText || node.nodeValue)?.toLowerCase()?.trim());
     const backwardoverlap = checkstroverlap((node.innerText || node.nodeValue)?.toLowerCase()?.trim(), find.og_text.toLowerCase().trim());
 
-    if (node.innerText?.toLowerCase().includes(find.og_text.toLowerCase()) 
-    || node.nodeValue?.toLowerCase().includes(find.og_text.toLowerCase()) 
-    || forwardoverlap > 2 
-    || backwardoverlap > 2
+
+    let show_modal_with_book_details = (random_ass_id) => () => {
+      // 1. Get the piece of text with the book name
+      let textNode = document.getElementById(random_ass_id);
+
+      // 2. When the modal is active, stop us from producing another one
+      textNode.onmouseover = null;
+      let bookTitle = find.title;
+
+      // We precision place our modal to be right next to the text that the user moused over
+      let { top, left, height, ...rest } = textNode.getBoundingClientRect();
+      top += window.scrollY + height; // Y
+      left += window.scrollX;
+
+
+      // 3. Produce the modal
+      console.log({ top, left, ...rest })
+      const node = document.createElement("div");
+      node.classList.add("card");
+      node.setAttribute("style", `position: absolute; top: ${top}px; left: ${left}px; z-index: 100`);
+      node.id = `${random_ass_id}-modal`;
+      node.innerHTML = `
+          <button class="delete" style="position: absolute; top: 0; right: 0; margin: 5px;" id="${random_ass_id}-deletebtn"></button>
+          <div class="card-content">
+              <div class="media-content">
+                <p class="title is-4">${bookTitle}</p>
+                <p class="subtitle is-6" id="${random_ass_id}-authornames">${find.author || '<progress class="progress is-small" max="100">50%</progress>'}</p>
+              </div>
+
+              <div class="content">
+                Available at the following libraries:
+                <ul id="${random_ass_id}-librarylist">
+                  <progress class="progress is-small" max="100">50%</progress>
+                </ul>
+              </div>
+          </div>
+      `;
+
+      // Show the modal in the DOM. We don't want it to inherit any funny styles -> make it a child of `body`
+      document.body.appendChild(node);
+
+      // 4. Make the modal delete button actually delete the modal
+      document.getElementById(`${random_ass_id}-deletebtn`).onclick = function () {
+        console.log("deleting ")
+        document.body.removeChild(node);
+        textNode.onmouseover = show_modal_with_book_details(random_ass_id);
+      };
+
+      // 5. Make request to backend to get book details
+      (async () => {
+        let res = await fetch(`${URL}/book_details`, {
+          method: "POST", // *GET, POST, PUT, DELETE, etc.
+          body: JSON.stringify({
+            book_title: bookTitle,
+            zipcode
+          }), // body data type must match "Content-Type" header,
+          ...common_fetch_params
+        });
+        let json = await res.json();
+
+        let book = json[0];
+        document.getElementById(`${random_ass_id}-authornames`).innerHTML = book.book_author;
+        document.getElementById(`${random_ass_id}-librarylist`).innerHTML = book.available_at.map((library) => {
+          let borrowhtml = '';
+          if (library.link_to_borrow) {
+            borrowhtml = `<a href="${library.link_to_borrow}" target="_blank" rel="noopener noreferrer"> Borrow :3 </a>`;
+          }
+          return `<li>${library.library_name} (${library.distance_str}) ${borrowhtml}</li>`;
+        }).join("");
+      }
+      )();
+
+    };
+
+    let random_ass_id = "xxxxxxxxxxxxxxx".replace(/x/g, () => (Math.floor(Math.random() * 36)).toString(36));
+  
+    if (node.innerText?.toLowerCase().includes(find.og_text.toLowerCase()) || node.nodeValue?.toLowerCase().includes(find.og_text.toLowerCase())
     ) {
-      if(find.og_text.includes("Pilgrim")) { console.log(find.og_text, node, node.innerText || node.nodeValue) }
       if (node.nodeType === Node.TEXT_NODE) {
         let bigger_overlap = Math.max(forwardoverlap, backwardoverlap);
         let is_forwards = bigger_overlap === forwardoverlap; // forwards means that og text is to left of node text
         // backwards means that node text is left of og text
-        let nodeText = node.nodeValue;        
-        let random_ass_id = "xxxxxxxxxxxxxxx".replace(/x/g, () => (Math.floor(Math.random() * 36)).toString(36));
-        
+        let nodeText = node.nodeValue;
         let boldText = `<span style='background-color: pink' id='${random_ass_id}'>${find.og_text}</span>`;
         let newHtml = nodeText.toLowerCase().replace(find.og_text.toLowerCase(), boldText);
         if (newHtml == nodeText.toLowerCase()) {
 
           let front = is_forwards ? "" : nodeText;
           let back = is_forwards ? nodeText : "";
-  
+
           // last overlap letters of front == first overlap letters of back
-          
+
           let to_highlight = back.substring(0, bigger_overlap);
-  
+
           let boldText = `${front.substring(0, front.length - bigger_overlap)}<span style='background-color: pink' id='${random_ass_id}'>${to_highlight}</span>${back.substring(bigger_overlap)}`;
           newHtml = boldText;
         }
@@ -81,87 +151,19 @@ window.onload = async (event) => {
         newNode.innerHTML = newHtml;
         parent.replaceChild(newNode, node);
 
-        let show_modal_with_book_details = () => {
-          // 1. Get the piece of text with the book name
-          let textNode = document.getElementById(random_ass_id);
-
-          // 2. When the modal is active, stop us from producing another one
-          textNode.onmouseover = null;
-          let bookTitle = find.title;
-
-          // We precision place our modal to be right next to the text that the user moused over
-          let { top, left, height, ...rest } = textNode.getBoundingClientRect();
-          top += window.scrollY + height; // Y
-          left += window.scrollX;
-
-
-          // 3. Produce the modal
-          console.log({ top, left, ...rest })
-          const node = document.createElement("div");
-          node.classList.add("card");
-          node.setAttribute("style", `position: absolute; top: ${top}px; left: ${left}px; z-index: 100`);
-          node.id = `${random_ass_id}-modal`;
-          node.innerHTML = `
-              <button class="delete" style="position: absolute; top: 0; right: 0; margin: 5px;" id="${random_ass_id}-deletebtn"></button>
-              <div class="card-content">
-                  <div class="media-content">
-                    <p class="title is-4">${bookTitle}</p>
-                    <p class="subtitle is-6" id="${random_ass_id}-authornames">${find.author || '<progress class="progress is-small" max="100">50%</progress>'}</p>
-                  </div>
-
-                  <div class="content">
-                    Available at the following libraries:
-                    <ul id="${random_ass_id}-librarylist">
-                      <progress class="progress is-small" max="100">50%</progress>
-                    </ul>
-                  </div>
-              </div>
-          `;
-
-          // Show the modal in the DOM. We don't want it to inherit any funny styles -> make it a child of `body`
-          document.body.appendChild(node);
-
-          // 4. Make the modal delete button actually delete the modal
-          document.getElementById(`${random_ass_id}-deletebtn`).onclick = function () {
-            console.log("deleting ")
-            document.body.removeChild(node);
-            textNode.onmouseover = show_modal_with_book_details;
-          };
-
-          // 5. Make request to backend to get book details
-          (async () => {
-            let res = await fetch(`${URL}/book_details`, {
-              method: "POST", // *GET, POST, PUT, DELETE, etc.
-              body: JSON.stringify({
-                book_title: bookTitle,
-                zipcode
-              }), // body data type must match "Content-Type" header,
-              ...common_fetch_params
-            });
-            let json = await res.json();
-
-            let book = json[0];
-            document.getElementById(`${random_ass_id}-authornames`).innerHTML = book.book_author;
-            document.getElementById(`${random_ass_id}-librarylist`).innerHTML = book.available_at.map((library) => {
-              let borrowhtml = '';
-              if (library.link_to_borrow) {
-                borrowhtml = `<a href="${library.link_to_borrow}" target="_blank" rel="noopener noreferrer"> Borrow :3 </a>`;
-              }
-              return `<li>${library.library_name} (${library.distance_str}) ${borrowhtml}</li>`;
-            }).join("");
-          }
-          )();
-
-        };
-        
-        setTimeout(() => document.getElementById(random_ass_id).onmouseover = show_modal_with_book_details, 500)
+        setTimeout(() => document.getElementById(random_ass_id).onmouseover = show_modal_with_book_details(random_ass_id), 500)
+        return true;
       } else {
-
-        for (const child of node.childNodes) {
-          recursive_find_replace(child, find, node)
+        let any_contain_it = [...node.childNodes].map((x) => recursive_find_replace(x, find, node)).some((x) => !!x);
+        if (!any_contain_it) {
+          node.style['background-color'] = "pink";
+          node.id = random_ass_id;
+          node.onmouseover = show_modal_with_book_details(random_ass_id)
         }
+        return true;
       }
     }
+    return false;
   };
 
   for (const uwu of respdata) {
