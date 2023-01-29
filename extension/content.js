9f703b51aea059fbb7017ec97e91bac35241a19f
Kanishk-K <kanishkkacholia@gmail.com>
2023-01-28 20:32:13 -0600

    Added extension to send dom data

---
+++ extension/content.js
@@ -0,0 +1,19 @@
+// Listen for messages
+console.log("Parsing Site Info for Library Books.");
+
+window.onload = (event) => {
+  const data = { text: document.body.innerText };
+  fetch("https://localhost:8000/extract_titles", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  }).then((response) => response.json()).then((data) => console.log(data));
+};
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  optimizeFonts: false,
+  distDir: 'build',
   images:{
     unoptimized: true
   }
