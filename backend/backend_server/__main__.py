b9b4afa2a7808745001e2e493bc50b16043e7352
Ritik Mishra <ritik.mishra314@gmail.com>
2023-01-29 05:51:45 -0600

    fix port

--- backend/backend_server/__main__.py
+++ backend/backend_server/__main__.py
@@ -2,4 +2,4 @@ import uvicorn
 import backend_server.app as app
 import os
 
-uvicorn.run(app.app, host="0.0.0.0", port=int(os.environ.get("BACKEND_PORT", 8000)))
+uvicorn.run(app.app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
