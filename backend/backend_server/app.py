ea32f1b703d6eacf60ca7a3e703d8e62a1c7efbb
Ritik Mishra <ritik.mishra314@gmail.com>
2023-01-28 20:28:02 -0600

    ya!

--- backend/backend_server/app.py
+++ backend/backend_server/app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from .title_extraction import extract_book_titles
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return "Hello World!"
+
+class ExtractTextRequest(BaseModel):
+    text: str
+
+@app.post("/extract_text")
+def extract_text(request: ExtractTextRequest) -> list[str]:
+    return extract_book_titles(request.text)
